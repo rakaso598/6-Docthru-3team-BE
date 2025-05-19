f186f1235641db85f9d35a51227ab53bb6ff1d4d
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-19 18:59:52 +0900

    create /auth/refresh-token

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import userRouter from "./routes/user.route.js";
 import challengeRouter from "./routes/challenge.route.js";
 import workRouter from "./routes/work.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
--- src/middlewares/accessToken.utils.js
+++ src/middlewares/accessToken.utils.js
@@ -18,3 +18,22 @@ export function generateAccessToken(user) {
 
   return accessToken;
 }
+
+export function generateRefreshToken(user) {
+  const payload = {
+    userId: user.id,
+    email: user.email,
+    nickname: user.nickname,
+  };
+
+  const refreshSecret = `${process.env.JWT_REFRESH_SECRET_KEY}`;
+
+  if (!refreshSecret) {
+    console.error("SECRET_KEY가 .env 파일에 없습니다.");
+    throw new Error("시크릿키를 확인하세요");
+  }
+
+  const refreshToken = jwt.sign(payload, refreshSecret, { expiresIn: "2w" });
+
+  return refreshToken;
+}
