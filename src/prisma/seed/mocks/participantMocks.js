6af466724185c1c1c48f4796c0abfa1077d5d9e9
fiivxyxxng <scx200800@gmail.com>
2025-05-20 18:35:22 +0900

    fix: 시딩 오류 해결

--- src/prisma/seed/mocks/participantMocks.js
+++ src/prisma/seed/mocks/participantMocks.js
@@ -1,302 +1,241 @@
-// participantMocks.js
 export const participantMocks = [
   {
-    id: 1,
     userId: "user13",
     challengeId: 1,
   },
   {
-    id: 2,
     userId: "user10",
     challengeId: 1,
   },
   {
-    id: 3,
     userId: "user15",
     challengeId: 1,
   },
   {
-    id: 4,
     userId: "user7",
     challengeId: 2,
   },
   {
-    id: 5,
     userId: "user15",
     challengeId: 2,
   },
   {
-    id: 6,
     userId: "user5",
     challengeId: 2,
   },
   {
-    id: 7,
     userId: "user2",
     challengeId: 3,
   },
   {
-    id: 8,
     userId: "user1",
     challengeId: 3,
   },
   {
-    id: 9,
     userId: "user4",
     challengeId: 3,
   },
   {
-    id: 10,
     userId: "user19",
     challengeId: 4,
   },
   {
-    id: 11,
     userId: "user6",
     challengeId: 4,
   },
   {
-    id: 12,
     userId: "user2",
     challengeId: 4,
   },
   {
-    id: 13,
     userId: "user13",
     challengeId: 5,
   },
   {
-    id: 14,
     userId: "user8",
     challengeId: 5,
   },
   {
-    id: 15,
     userId: "user10",
     challengeId: 5,
   },
   {
-    id: 16,
     userId: "user2",
     challengeId: 6,
   },
   {
-    id: 17,
     userId: "user13",
     challengeId: 6,
   },
   {
-    id: 18,
     userId: "user15",
     challengeId: 6,
   },
   {
-    id: 19,
     userId: "user8",
     challengeId: 7,
   },
   {
-    id: 20,
     userId: "user6",
     challengeId: 7,
   },
   {
-    id: 21,
     userId: "user3",
     challengeId: 7,
   },
   {
-    id: 22,
     userId: "user8",
     challengeId: 8,
   },
   {
-    id: 23,
     userId: "user10",
     challengeId: 8,
   },
   {
-    id: 24,
     userId: "user17",
     challengeId: 8,
   },
   {
-    id: 25,
     userId: "user20",
     challengeId: 9,
   },
   {
-    id: 26,
     userId: "user16",
     challengeId: 9,
   },
   {
-    id: 27,
     userId: "user18",
     challengeId: 9,
   },
   {
-    id: 28,
     userId: "user8",
     challengeId: 10,
   },
   {
-    id: 29,
     userId: "user20",
     challengeId: 10,
   },
   {
-    id: 30,
     userId: "user14",
     challengeId: 10,
   },
   {
-    id: 31,
     userId: "user4",
     challengeId: 11,
   },
   {
-    id: 32,
     userId: "user16",
     challengeId: 11,
   },
   {
-    id: 33,
     userId: "user20",
     challengeId: 11,
   },
   {
-    id: 34,
     userId: "user10",
     challengeId: 12,
   },
   {
-    id: 35,
     userId: "user6",
     challengeId: 12,
   },
   {
-    id: 36,
     userId: "user14",
     challengeId: 12,
   },
   {
-    id: 37,
     userId: "user17",
     challengeId: 13,
   },
   {
-    id: 38,
     userId: "user5",
     challengeId: 13,
   },
   {
-    id: 39,
     userId: "user18",
     challengeId: 13,
   },
   {
-    id: 40,
     userId: "user13",
     challengeId: 14,
   },
   {
-    id: 41,
     userId: "user9",
     challengeId: 14,
   },
   {
-    id: 42,
     userId: "user2",
     challengeId: 14,
   },
   {
-    id: 43,
     userId: "user15",
     challengeId: 15,
   },
   {
-    id: 44,
     userId: "user14",
     challengeId: 15,
   },
   {
-    id: 45,
     userId: "user13",
     challengeId: 15,
   },
   {
-    id: 46,
     userId: "user5",
     challengeId: 16,
   },
   {
-    id: 47,
     userId: "user8",
     challengeId: 16,
   },
   {
-    id: 48,
     userId: "user18",
     challengeId: 16,
   },
   {
-    id: 49,
     userId: "user11",
     challengeId: 17,
   },
   {
-    id: 50,
     userId: "user8",
     challengeId: 17,
   },
   {
-    id: 51,
     userId: "user6",
     challengeId: 17,
   },
   {
-    id: 52,
     userId: "user13",
     challengeId: 18,
   },
   {
-    id: 53,
     userId: "user19",
     challengeId: 18,
   },
   {
-    id: 54,
     userId: "user8",
     challengeId: 18,
   },
   {
-    id: 55,
     userId: "user3",
     challengeId: 19,
   },
   {
-    id: 56,
     userId: "user8",
     challengeId: 19,
   },
   {
-    id: 57,
     userId: "user14",
     challengeId: 19,
   },
   {
-    id: 58,
     userId: "user16",
     challengeId: 20,
   },
   {
-    id: 59,
     userId: "user13",
     challengeId: 20,
   },
   {
-    id: 60,
     userId: "user5",
     challengeId: 20,
   },
