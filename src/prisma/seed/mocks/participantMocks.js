00f2ce302b24fba1d4d7d74759b425c2fb17fc85
fiivxyxxng <scx200800@gmail.com>
2025-05-20 15:12:57 +0900

    feat: 시드데이터 추가

---
+++ src/prisma/seed/mocks/participantMocks.js
@@ -0,0 +1,172 @@
+export const participantMocks = [
+  {
+    id: 1,
+    userId: "c7db18b3d84014992b20176b",
+    challengeId: 1,
+  },
+  {
+    id: 2,
+    userId: "c8a7b58af93d74f9a8ce5bdc",
+    challengeId: 1,
+  },
+  {
+    id: 3,
+    userId: "cc73ff3ac9bcd4b59a162e82",
+    challengeId: 1,
+  },
+  {
+    id: 4,
+    userId: "cfea986a119be414dae0b918",
+    challengeId: 1,
+  },
+  {
+    id: 5,
+    userId: "cd301485be2e3462b8d0c20f",
+    challengeId: 1,
+  },
+  {
+    id: 6,
+    userId: "cba2d1a8c5ffc47c294ed946",
+    challengeId: 1,
+  },
+  {
+    id: 7,
+    userId: "c46eabd7a9db64623a870b73",
+    challengeId: 1,
+  },
+  {
+    id: 8,
+    userId: "ccfa993ff933a40deaa87768",
+    challengeId: 1,
+  },
+  {
+    id: 9,
+    userId: "c71d41fc8e5594cef8aca722",
+    challengeId: 2,
+  },
+  {
+    id: 10,
+    userId: "cbbd08e48ce35402885b6d85",
+    challengeId: 2,
+  },
+  {
+    id: 11,
+    userId: "cd301485be2e3462b8d0c20f",
+    challengeId: 2,
+  },
+  {
+    id: 12,
+    userId: "cde13dcd2e58b48d6820fd47",
+    challengeId: 2,
+  },
+  {
+    id: 13,
+    userId: "cde13dcd2e58b48d6820fd47",
+    challengeId: 3,
+  },
+  {
+    id: 14,
+    userId: "c46eabd7a9db64623a870b73",
+    challengeId: 3,
+  },
+  {
+    id: 15,
+    userId: "c71d41fc8e5594cef8aca722",
+    challengeId: 3,
+  },
+  {
+    id: 16,
+    userId: "cba2d1a8c5ffc47c294ed946",
+    challengeId: 3,
+  },
+  {
+    id: 17,
+    userId: "cfea986a119be414dae0b918",
+    challengeId: 3,
+  },
+  {
+    id: 18,
+    userId: "cd301485be2e3462b8d0c20f",
+    challengeId: 3,
+  },
+  {
+    id: 19,
+    userId: "c8a7b58af93d74f9a8ce5bdc",
+    challengeId: 3,
+  },
+  {
+    id: 20,
+    userId: "cc73ff3ac9bcd4b59a162e82",
+    challengeId: 3,
+  },
+  {
+    id: 21,
+    userId: "cdff06d99d01340a08a325cf",
+    challengeId: 4,
+  },
+  {
+    id: 22,
+    userId: "cde13dcd2e58b48d6820fd47",
+    challengeId: 4,
+  },
+  {
+    id: 23,
+    userId: "cbbd08e48ce35402885b6d85",
+    challengeId: 4,
+  },
+  {
+    id: 24,
+    userId: "ca0fc9dc405194dc5a4a1872",
+    challengeId: 5,
+  },
+  {
+    id: 25,
+    userId: "c31d064aa9a8a4ea394d58fc",
+    challengeId: 5,
+  },
+  {
+    id: 26,
+    userId: "c8a7b58af93d74f9a8ce5bdc",
+    challengeId: 6,
+  },
+  {
+    id: 27,
+    userId: "cbbd08e48ce35402885b6d85",
+    challengeId: 6,
+  },
+  {
+    id: 28,
+    userId: "cba2d1a8c5ffc47c294ed946",
+    challengeId: 6,
+  },
+  {
+    id: 29,
+    userId: "c7db18b3d84014992b20176b",
+    challengeId: 6,
+  },
+  {
+    id: 30,
+    userId: "ca0fc9dc405194dc5a4a1872",
+    challengeId: 6,
+  },
+  {
+    id: 31,
+    userId: "cba2d1a8c5ffc47c294ed946",
+    challengeId: 7,
+  },
+  {
+    id: 32,
+    userId: "ccfa993ff933a40deaa87768",
+    challengeId: 7,
+  },
+  {
+    id: 33,
+    userId: "c31d064aa9a8a4ea394d58fc",
+    challengeId: 7,
+  },
+  {
+    id: 34,
+    userId: "c21b66b9fe18943aabd88271",
+    challengeId: 7,
+  },
+];
