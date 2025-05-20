e198521ade7e241a0f5a6be1d489abc2589ae605
fiivxyxxng <scx200800@gmail.com>
2025-05-20 18:35:22 +0900

    fix: 시딩 오류 해결

--- src/prisma/seed/mocks/likeMocks.js
+++ src/prisma/seed/mocks/likeMocks.js
@@ -1,767 +1,613 @@
-// likeMocks.js
 export const likeMocks = [
   {
-    id: 1,
     userId: "user5",
     workId: 1,
   },
   {
-    id: 2,
     userId: "user2",
     workId: 1,
   },
   {
-    id: 3,
     userId: "user11",
     workId: 1,
   },
   {
-    id: 4,
     userId: "user13",
     workId: 2,
   },
   {
-    id: 5,
     userId: "user8",
     workId: 2,
   },
   {
-    id: 6,
     userId: "user6",
     workId: 2,
   },
   {
-    id: 7,
     userId: "user10",
     workId: 3,
   },
   {
-    id: 8,
     userId: "user8",
     workId: 3,
   },
   {
-    id: 9,
     userId: "user3",
     workId: 3,
   },
   {
-    id: 10,
     userId: "user9",
     workId: 4,
   },
   {
-    id: 11,
     userId: "user3",
     workId: 4,
   },
   {
-    id: 12,
     userId: "user15",
     workId: 4,
   },
   {
-    id: 13,
     userId: "user8",
     workId: 5,
   },
   {
-    id: 14,
     userId: "user10",
     workId: 5,
   },
   {
-    id: 15,
     userId: "user18",
     workId: 5,
   },
   {
-    id: 16,
     userId: "user6",
     workId: 6,
   },
   {
-    id: 17,
     userId: "user14",
     workId: 6,
   },
   {
-    id: 18,
     userId: "user8",
     workId: 6,
   },
   {
-    id: 19,
     userId: "user19",
     workId: 7,
   },
   {
-    id: 20,
     userId: "user15",
     workId: 7,
   },
   {
-    id: 21,
     userId: "user17",
     workId: 7,
   },
   {
-    id: 22,
     userId: "user9",
     workId: 8,
   },
   {
-    id: 23,
     userId: "user13",
     workId: 8,
   },
   {
-    id: 24,
     userId: "user8",
     workId: 8,
   },
   {
-    id: 25,
     userId: "user2",
     workId: 9,
   },
   {
-    id: 26,
     userId: "user17",
     workId: 9,
   },
   {
-    id: 27,
     userId: "user16",
     workId: 9,
   },
   {
-    id: 28,
     userId: "user15",
     workId: 10,
   },
   {
-    id: 29,
     userId: "user17",
     workId: 10,
   },
   {
-    id: 30,
     userId: "user16",
     workId: 10,
   },
   {
-    id: 31,
     userId: "user2",
     workId: 11,
   },
   {
-    id: 32,
     userId: "user20",
     workId: 11,
   },
   {
-    id: 33,
     userId: "user12",
     workId: 11,
   },
   {
-    id: 34,
     userId: "user1",
     workId: 12,
   },
   {
-    id: 35,
     userId: "user5",
     workId: 12,
   },
   {
-    id: 36,
     userId: "user15",
     workId: 12,
   },
   {
-    id: 37,
     userId: "user18",
     workId: 13,
   },
   {
-    id: 38,
     userId: "user2",
     workId: 13,
   },
   {
-    id: 39,
     userId: "user17",
     workId: 13,
   },
   {
-    id: 40,
     userId: "user8",
     workId: 14,
   },
   {
-    id: 41,
     userId: "user4",
     workId: 14,
   },
   {
-    id: 42,
     userId: "user9",
     workId: 14,
   },
   {
-    id: 43,
     userId: "user3",
     workId: 15,
   },
   {
-    id: 44,
     userId: "user17",
     workId: 15,
   },
   {
-    id: 45,
     userId: "user11",
     workId: 15,
   },
   {
-    id: 46,
     userId: "user1",
     workId: 16,
   },
   {
-    id: 47,
     userId: "user4",
     workId: 16,
   },
   {
-    id: 48,
     userId: "user7",
     workId: 16,
   },
   {
-    id: 49,
     userId: "user6",
     workId: 17,
   },
   {
-    id: 50,
     userId: "user16",
     workId: 17,
   },
   {
-    id: 51,
     userId: "user14",
     workId: 17,
   },
   {
-    id: 52,
     userId: "user9",
     workId: 18,
   },
   {
-    id: 53,
     userId: "user20",
     workId: 18,
   },
   {
-    id: 54,
     userId: "user5",
     workId: 18,
   },
   {
-    id: 55,
     userId: "user10",
     workId: 19,
   },
   {
-    id: 56,
     userId: "user12",
     workId: 19,
   },
   {
-    id: 57,
     userId: "user11",
     workId: 19,
   },
   {
-    id: 58,
     userId: "user19",
     workId: 20,
   },
   {
-    id: 59,
     userId: "user17",
     workId: 20,
   },
   {
-    id: 60,
     userId: "user4",
     workId: 20,
   },
   {
-    id: 61,
     userId: "user20",
     workId: 21,
   },
   {
-    id: 62,
     userId: "user6",
     workId: 21,
   },
   {
-    id: 63,
     userId: "user11",
     workId: 21,
   },
   {
-    id: 64,
     userId: "user12",
     workId: 22,
   },
   {
-    id: 65,
     userId: "user17",
     workId: 22,
   },
   {
-    id: 66,
     userId: "user5",
     workId: 22,
   },
   {
-    id: 67,
     userId: "user18",
     workId: 23,
   },
   {
-    id: 68,
     userId: "user12",
     workId: 23,
   },
   {
-    id: 69,
     userId: "user3",
     workId: 23,
   },
   {
-    id: 70,
     userId: "user16",
     workId: 24,
   },
   {
-    id: 71,
     userId: "user9",
     workId: 24,
   },
   {
-    id: 72,
     userId: "user18",
     workId: 24,
   },
   {
-    id: 73,
     userId: "user19",
     workId: 25,
   },
   {
-    id: 74,
     userId: "user17",
     workId: 25,
   },
   {
-    id: 75,
     userId: "user8",
     workId: 25,
   },
   {
-    id: 76,
     userId: "user5",
     workId: 26,
   },
   {
-    id: 77,
     userId: "user8",
     workId: 26,
   },
   {
-    id: 78,
     userId: "user18",
     workId: 26,
   },
   {
-    id: 79,
     userId: "user10",
     workId: 27,
   },
   {
-    id: 80,
     userId: "user18",
     workId: 27,
   },
   {
-    id: 81,
     userId: "user13",
     workId: 27,
   },
   {
-    id: 82,
     userId: "user7",
     workId: 28,
   },
   {
-    id: 83,
     userId: "user12",
     workId: 28,
   },
   {
-    id: 84,
     userId: "user15",
     workId: 28,
   },
   {
-    id: 85,
     userId: "user2",
     workId: 29,
   },
   {
-    id: 86,
     userId: "user6",
     workId: 29,
   },
   {
-    id: 87,
     userId: "user3",
     workId: 29,
   },
   {
-    id: 88,
     userId: "user15",
     workId: 30,
   },
   {
-    id: 89,
     userId: "user5",
     workId: 30,
   },
   {
-    id: 90,
     userId: "user10",
     workId: 30,
   },
   {
-    id: 91,
     userId: "user2",
     workId: 31,
   },
   {
-    id: 92,
     userId: "user19",
     workId: 31,
   },
   {
-    id: 93,
     userId: "user6",
     workId: 31,
   },
   {
-    id: 94,
     userId: "user5",
     workId: 32,
   },
   {
-    id: 95,
     userId: "user3",
     workId: 32,
   },
   {
-    id: 96,
     userId: "user4",
     workId: 32,
   },
   {
-    id: 97,
     userId: "user17",
     workId: 33,
   },
   {
-    id: 98,
     userId: "user19",
     workId: 33,
   },
   {
-    id: 99,
     userId: "user10",
     workId: 33,
   },
   {
-    id: 100,
     userId: "user15",
     workId: 34,
   },
   {
-    id: 101,
     userId: "user17",
     workId: 34,
   },
   {
-    id: 102,
     userId: "user14",
     workId: 34,
   },
   {
-    id: 103,
     userId: "user16",
     workId: 35,
   },
   {
-    id: 104,
     userId: "user6",
     workId: 35,
   },
   {
-    id: 105,
     userId: "user5",
     workId: 35,
   },
   {
-    id: 106,
     userId: "user20",
     workId: 36,
   },
   {
-    id: 107,
     userId: "user9",
     workId: 36,
   },
   {
-    id: 108,
     userId: "user19",
     workId: 36,
   },
   {
-    id: 109,
     userId: "user20",
     workId: 37,
   },
   {
-    id: 110,
     userId: "user13",
     workId: 37,
   },
   {
-    id: 111,
     userId: "user3",
     workId: 37,
   },
   {
-    id: 112,
     userId: "user19",
     workId: 38,
   },
   {
-    id: 113,
     userId: "user3",
     workId: 38,
   },
   {
-    id: 114,
     userId: "user10",
     workId: 38,
   },
   {
-    id: 115,
     userId: "user20",
     workId: 39,
   },
   {
-    id: 116,
     userId: "user17",
     workId: 39,
   },
   {
-    id: 117,
     userId: "user14",
     workId: 39,
   },
   {
-    id: 118,
     userId: "user2",
     workId: 40,
   },
   {
-    id: 119,
     userId: "user20",
     workId: 40,
   },
   {
-    id: 120,
     userId: "user4",
     workId: 40,
   },
   {
-    id: 121,
     userId: "user5",
     workId: 41,
   },
   {
-    id: 122,
     userId: "user2",
     workId: 41,
   },
   {
-    id: 123,
     userId: "user16",
     workId: 41,
   },
   {
-    id: 124,
     userId: "user15",
     workId: 42,
   },
   {
-    id: 125,
     userId: "user1",
     workId: 42,
   },
   {
-    id: 126,
     userId: "user10",
     workId: 42,
   },
   {
-    id: 127,
     userId: "user19",
     workId: 43,
   },
   {
-    id: 128,
     userId: "user9",
     workId: 43,
   },
   {
-    id: 129,
     userId: "user3",
     workId: 43,
   },
   {
-    id: 130,
     userId: "user2",
     workId: 44,
   },
   {
-    id: 131,
     userId: "user10",
     workId: 44,
   },
   {
-    id: 132,
     userId: "user11",
     workId: 44,
   },
   {
-    id: 133,
     userId: "user13",
     workId: 45,
   },
   {
-    id: 134,
     userId: "user20",
     workId: 45,
   },
   {
-    id: 135,
     userId: "user6",
     workId: 45,
   },
   {
-    id: 136,
     userId: "user8",
     workId: 46,
   },
   {
-    id: 137,
     userId: "user20",
     workId: 46,
   },
   {
-    id: 138,
     userId: "user9",
     workId: 46,
   },
   {
-    id: 139,
     userId: "user10",
     workId: 47,
   },
   {
-    id: 140,
     userId: "user12",
     workId: 47,
   },
   {
-    id: 141,
     userId: "user13",
     workId: 47,
   },
   {
-    id: 142,
     userId: "user14",
     workId: 48,
   },
   {
-    id: 143,
     userId: "user4",
     workId: 48,
   },
   {
-    id: 144,
     userId: "user15",
     workId: 48,
   },
   {
-    id: 145,
     userId: "user8",
     workId: 49,
   },
   {
-    id: 146,
     userId: "user4",
     workId: 49,
   },
   {
-    id: 147,
     userId: "user16",
     workId: 49,
   },
   {
-    id: 148,
     userId: "user11",
     workId: 50,
   },
   {
-    id: 149,
     userId: "user5",
     workId: 50,
   },
   {
-    id: 150,
     userId: "user7",
     workId: 50,
   },
   {
-    id: 151,
     userId: "user5",
     workId: 51,
   },
   {
-    id: 152,
     userId: "user7",
     workId: 51,
   },
   {
-    id: 153,
     userId: "user12",
     workId: 51,
   },
