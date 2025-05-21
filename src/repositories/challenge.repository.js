2a46274141c682c6a686485f9de884a7e7507f0a
fiivxyxxng <scx200800@gmail.com>
2025-05-21 20:33:05 +0900

    feat: 어드민 챌린지 신청 관리 기능 구현

--- src/repositories/challenge.repository.js
+++ src/repositories/challenge.repository.js
@@ -1,27 +1,49 @@
 import prisma from "../prisma/client.prisma.js";
 
+/**
+ * 챌린지 등록 및 자동 신청
+ */
 async function save(challenge, userId) {
-  return await prisma.challenge.create({
-    data: {
-      authorId: userId,
-      title: challenge.title,
-      description: challenge.description,
-      category: challenge.category,
-      docType: challenge.docType,
-      originalUrl: challenge.originalUrl,
-      deadline: challenge.deadline,
-      maxParticipant: challenge.maxParticipant,
-    },
+  return await prisma.$transaction(async (tx) => {
+    const createdChallenge = await tx.challenge.create({
+      data: {
+        authorId: userId,
+        title: challenge.title,
+        description: challenge.description,
+        category: challenge.category,
+        docType: challenge.docType,
+        originalUrl: challenge.originalUrl,
+        deadline: challenge.deadline,
+        maxParticipant: challenge.maxParticipant,
+      },
+    });
+
+    const createdApplication = await tx.application.create({
+      data: {
+        authorId: userId,
+        challengeId: createdChallenge.id,
+        appliedAt: new Date(),
+      },
+    });
+
+    return { createdChallenge, createdApplication };
   });
 }
 
-// 모든 challenge 조회
+// 승인된 챌린지 전체 조회
 const findAllChallenges = async () => {
-  return await prisma.challenge.findMany ({})
-}
+  return await prisma.challenge.findMany({
+    where: {
+      application: {
+        adminStatus: "ACCEPTED",
+      },
+    },
+  });
+};
+
 // 특정 challenge 조회 (상세 조회에 활용)
 const findChallengeDetailById = async (challengeId) => {
-  return await prisma.challenge.findUnique ( {
+  return await prisma.challenge.findUnique({
     where: { id: challengeId },
     select: {
       id: true,
@@ -34,36 +56,46 @@ const findChallengeDetailById = async (challengeId) => {
       maxParticipant: true,
       authorId: true, // 필요 시
     },
-  })
-}
+  });
+};
 
 // 특정 challenge 조회  (수정, 삭제에 활용)
 const findChallengeById = async (challengeId) => {
-  return await prisma.challenge.findUnique ( {
+  return await prisma.challenge.findUnique({
     where: { id: challengeId },
     select: { id: true, authorId: true },
-  })
-}
+  });
+};
 
 // challenge update
 const updateChallenge = async (challengeId, updateData) => {
   return await prisma.challenge.update({
     where: { id: challengeId },
     data: updateData,
-  })
-}
+  });
+};
 
-const deleteChallengeById = async (challengeId) => {  
+// 관리자 - 신청 상태 업데이트
+const updateApplication = async (challengeId, updateData) => {
+  return await prisma.application.update({
+    where: { challengeId },
+    data: {
+      ...updateData,
+      invalidatedAt: new Date(),
+    },
+  });
+};
+
+const deleteChallengeById = async (challengeId) => {
   await prisma.application.deleteMany({
     where: {
-      challengeId : challengeId, // 해당 챌린지를 참조하는 모든 신청 삭제
-      },
+      challengeId: challengeId, // 해당 챌린지를 참조하는 모든 신청 삭제
+    },
   });
-  
+
   await prisma.challenge.delete({
     where: { id: challengeId },
   });
-
 };
 
 async function getChallenges(options) {
@@ -107,6 +139,7 @@ export default {
   findAllChallenges,
   findChallengeById,
   updateChallenge,
+  updateApplication,
   deleteChallengeById,
-  findChallengeDetailById
+  findChallengeDetailById,
 };
