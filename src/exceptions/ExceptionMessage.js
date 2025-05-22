2732ab5ff45f657a3e597a22a9fd6f4ac16415de
fiivxyxxng <scx200800@gmail.com>
2025-05-22 17:27:12 +0900

    modify: 로그인, 회원가입 관련 수정

--- src/exceptions/ExceptionMessage.js
+++ src/exceptions/ExceptionMessage.js
@@ -1,11 +1,18 @@
 export const ExceptionMessage = {
+  // Not Found 에러
   CHALLNEGE_NOT_FOUND: "챌린지를 찾을 수 없습니다.",
   WORK_NOT_FOUND: "작업물을 찾을 수 없습니다.",
   FEEDBACK_NOT_FOUND: "피드백을 찾을 수 없습니다.",
   USER_NOT_FOUND: "사용자를 찾을 수 없습니다.",
+
+  // 로그인, 회원가입 유효성 검사 에러
   PASSWORD_NOT_MATCH: "비밀번호가 일치하지 않습니다.",
   PASSWORD_CONFIRMATION_NOT_MATCH: "비밀번호 확인이 일치하지 않습니다.",
-  ALREADY_REGISTERED_EMAIL: "이미 등록된 이메일입니다.",
+  ALREADY_EXISTED_EMAIL: "이미 사용중인 이메일입니다.",
+  ALREADY_EXISTED_NICKNAME: "이미 사용중인 닉네임입니다.",
+  INVALID_EMAIL: "잘못된 이메일입니다.",
+  INVALID_INPUT: "필수 정보를 모두 입력해주세요.",
+
   UNAUTHORIZED: "인증되지 않은 사용자입니다.",
   FORBIDDEN: "요청한 작업을 수행하기 위한 권한이 없습니다.",
   INVALID_REFRESH_TOKEN: "유효하지 않은 리프레시 토큰입니다.",
