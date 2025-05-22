ecc77e4c39ad790281ef4d1f6ba0d8461601e6c8
suKyoung25 <s.kyoung5496@gmail.com>
2025-05-22 10:15:34 +0900

    arrange  challenge-route

--- src/routes/challenge.route.js
+++ src/routes/challenge.route.js
@@ -1,14 +1,30 @@
 import express from "express";
 import {
-  createChallenge,
   getChallenges,
+  createChallenge,
+  getChallengeById,
+  updateChallenge,
+  deleteChallenge,
 } from "../controllers/challenge.controller.js";
-import auth from "../middlewares/auth.js";
+import { verifyAccessToken } from "../middlewares/verifyToken.js";
+import workRouter from "./work.route.js";
 
 const challengeRouter = express.Router();
 
+// 챌린지 작업물 관련 라우터
+challengeRouter.use("/:challengeId/works", workRouter);
+
 // 챌린지 생성
-challengeRouter.post("/", auth.verifyAccessToken, createChallenge);
+challengeRouter.post("/", verifyAccessToken, createChallenge);
+
+// 챌린지 상세 조회
+challengeRouter.get("/:challengeId", getChallengeById);
+
+// 챌린지 수정
+challengeRouter.put("/:challengeId", verifyAccessToken, updateChallenge);
+
+// 챌린지 삭제
+challengeRouter.delete("/:challengeId", verifyAccessToken, deleteChallenge);
 
 //챌린지 목록 조회
 challengeRouter.get("/", getChallenges);
