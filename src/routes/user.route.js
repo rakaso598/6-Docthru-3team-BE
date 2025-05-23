8de980b0b59e9d65901b3316615d31e6296c776d
조성빈 <joseongbin@joseongbin-ui-MacBookAir.local>
2025-05-23 16:31:48 +0900

    fix:user api 코드 수정

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getMyInfo } from "../controllers/user.controller.js";
+import { getMyInfo, getMyChallenges } from "../controllers/user.controller.js";
 import { verifyAccessToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/me", verifyAccessToken, getMyInfo);
+
+router.get("/me/challenges", verifyAccessToken, getMyChallenges);
 
 export default router;
